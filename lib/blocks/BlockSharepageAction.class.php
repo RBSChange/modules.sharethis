dc9f0961614c673bd857a4994fb8a879491a269b
Franck STAUFFER <franck.stauffer@rbs.fr>
2010-08-17 08:33:59 +0200

    [NEW] Cache enabled for sharethis block

--- lib/blocks/BlockSharepageAction.class.php
+++ lib/blocks/BlockSharepageAction.class.php
@@ -6,6 +6,29 @@
 class sharethis_BlockSharepageAction extends website_BlockAction
 {
 	/**
+	 * @return array or null
+	 */
+	public function getCacheDependencies()
+	{
+		return array("modules_sharethis/link");
+	}
+	
+	/**
+	 * @see website_BlockAction::execute()
+	 *
+	 * @param f_mvc_Request $request
+	 * @param f_mvc_Response $response
+	 * @return String
+	 */
+	/**
+	 * @param unknown_type $request
+	 * @return multitype:
+	 */
+	public function getCacheKeyParameters($request)
+	{
+		return array($_SERVER['REQUEST_URI']);
+	}
+	/**
 	 * @see website_BlockAction::execute()
 	 *
 	 * @param f_mvc_Request $request
