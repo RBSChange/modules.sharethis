fd419f5d77b89c993153da84c2340c36ec002997
Gaël PORT <gael.port@rbs.fr>
2011-02-23 09:36:58 +0100

    [REFACTOR] Move addTreeAttributes and addFormProperties from final document class to document service.

--- persistentdocument/group.class.php
+++ persistentdocument/group.class.php
@@ -5,20 +5,4 @@
  */
 class sharethis_persistentdocument_group extends sharethis_persistentdocument_groupbase 
 {
-	/**
-	 * @param string $moduleName
-	 * @param string $treeType
-	 * @param array<string, string> $nodeAttributes
-	 */
-//	protected function addTreeAttributes($moduleName, $treeType, &$nodeAttributes)
-//	{
-//	}
-	
-	/**
-	 * @param string $actionType
-	 * @param array $formProperties
-	 */
-//	public function addFormProperties($propertiesNames, &$formProperties)
-//	{	
-//	}
 }
