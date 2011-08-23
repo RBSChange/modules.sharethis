f6e4188756a16aab55ac91861586ca269c91b233
Franck STAUFFER <franck.stauffer@rbs.fr>
2011-08-23 11:41:12 +0200

    New class injection comptability

--- lib/services/BlogsiteService.class.php
+++ lib/services/BlogsiteService.class.php
@@ -17,7 +17,7 @@ class sharethis_BlogsiteService extends sharethis_SiteService
 	{
 		if (self::$instance === null)
 		{
-			self::$instance = self::getServiceClassInstance(get_class());
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
--- lib/services/SiteService.class.php
+++ lib/services/SiteService.class.php
@@ -17,7 +17,7 @@ class sharethis_SiteService extends sharethis_LinkService
 	{
 		if (self::$instance === null)
 		{
-			self::$instance = self::getServiceClassInstance(get_class());
+			self::$instance = new self();
 		}
 		return self::$instance;
 	}
