6955f31856b4b8864e9a6bce086bb29189600eab
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-04-04 10:31:33 +0200

    [UPDATE] Remove deprecated patch

--- patch/0360/install.php
+++ patch/0360/install.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * sharethis_patch_0352
+ * sharethis_patch_0360
  * @package modules.sharethis
  */
-class sharethis_patch_0352 extends patch_BasePatch
+class sharethis_patch_0360 extends patch_BasePatch
 {
 	/**
 	 * Entry point of the patch execution.
